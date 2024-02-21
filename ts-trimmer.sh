488567ba54b77d0eb8ac533c0c5fcd364dca936a
Alex Chalk <contact@alexchalk.net>
2024-02-20 23:24:36 -0500

    ts-trimmer: catch multi-footnote abbreviations

--- ts-trimmer.sh
+++ ts-trimmer.sh
@@ -24,7 +24,7 @@ fi
 
 tst() {
   local article
-  local title_matcher='\[([^\[]*?)[[:space:]]?\]\.$'
+  local title_matcher='\[([^\[]*?)[[:space:]]?\](;|\.$)'
   local short_titles
   local remove_hyphens
   local remove_linebreaks
