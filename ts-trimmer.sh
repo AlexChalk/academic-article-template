5648e1830e26e0d9d7c1086190d21175112a79a1
Alex Chalk <contact@alexchalk.net>
2024-02-20 21:15:49 -0500

    Squash some ts-trimmer bugs
    - Handle hyphenated title-shorts in footnotes
    - Only match against title-short use in footnotes (use supra)
    - Handle non-zero exit code for zero count matches
    - Exit early if nothing to remove
    - Improve cli output formatting

--- ts-trimmer.sh
+++ ts-trimmer.sh
@@ -26,7 +26,8 @@ tst() {
   local article
   local title_matcher='\[([^\[]*?)[[:space:]]?\]\.$'
   local short_titles
-  local titles_oneline
+  local remove_hyphens
+  local remove_linebreaks
   local titles_clean
   local titles_to_rm=()
   local json_titles
@@ -38,23 +39,31 @@ tst() {
   short_titles=$(echo "$article" | rg --multiline --only-matching "$title_matcher" --replace '[$1]')
 
   # Sed command: #39 at https://catonmat.net/sed-one-liners-explained-part-one (!N performs command for *non*-matching lines)
-  titles_oneline=$(echo "${short_titles[@]}" | sed --expression :a --expression '/\]$/!N; s/\n/\ /; ta')
+  remove_hyphens=$(echo "${short_titles[@]}" | sed --expression :a --expression '/-$/N; s/-\n//; ta')
+  remove_linebreaks=$(echo "${remove_hyphens[@]}" | sed --expression :a --expression '/\]$/!N; s/\n/\ /; ta')
 
-  titles_clean=$(echo "${titles_oneline[@]}" | tr --delete '^[' | tr --delete ']$')
+  titles_clean=$(echo "${remove_linebreaks[@]}" | tr --delete '^[' | tr --delete ']$')
 
   while read -r title; do
     local count
     local regexp_title
 
-    regexp_title="${title// /[[:space:]]}"
+    regexp_title="${title// /[[:space:]]}, supra"
 
-    count=$(echo "$article" | rg --multiline --count "$regexp_title")
+    count=$(echo "$article" | rg --multiline --count --include-zero "$regexp_title" || true)
 
-    if (("$count" <= 1)); then
+    if (("$count" == 0)); then
       titles_to_rm+=("$title")
     fi
   done < <(echo "${titles_clean[@]}")
 
+  if ((${#titles_to_rm[@]} == 0)); then
+    echo ""
+    echo "Nothing to remove; exiting"
+    exit
+  fi
+
+  echo ""
   echo "TS Trimmer will remove the following unused titles from" "$2"":"
   echo ""
 
